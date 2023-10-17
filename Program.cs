6af2333d7c60bad642c496695685fa631cc413d2
alec-tackitt-student <144161418+alec-tackitt-student@users.noreply.github.com>
2023-10-17 16:43:16 +0000

    themed and finished project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information'
-int RoomNumber = 1;
-int userRoom;
-int GhostRoom;
+using System.Security.Cryptography.X509Certificates;
+
+int Jnumber = 1;
+int userTurn;
+int WrongTurn;
 Random rnd = new Random();
 //startup user instructions
-Console.WriteLine("Welcome to Alec's haunted house!");
-Console.WriteLine("You must make it through all 10 sets of doors to survive!!");
-Console.WriteLine("though behind 1 door there is a Ghost!, if you go through the one with");
-Console.WriteLine("the ghost you will loose.");
+Console.WriteLine("Welcome to the CTC!");
+Console.WriteLine("You are almost late to class, and only 1 path through the school can");
+Console.WriteLine("get you to class in time, but at each of the 10 junctions there are 3 ways to go!");
+Console.WriteLine("If you make a wrong turn you will not make it to class in time and Mr.Martinez will give you a tardy.");
+Console.WriteLine("If you sucessfly make it to class in time Mr.Martinez will give the class cookies.");
+Console.WriteLine("Good luck studnet.");
 
 // functions
 static void ClearLine(){
@@ -16,42 +20,48 @@ static void ClearLine(){
     Console.SetCursorPosition(0, Console.CursorTop - 1);
 }
 
-void Entrypoint() {
+//main loop
+while (Jnumber <= 10)  {
     Console.WriteLine("");
-    Console.WriteLine("you are on room #" + RoomNumber);
-    Console.WriteLine("3 doors lie ahead...");
-    Console.WriteLine("There's a ghost behind one.");
+    Console.WriteLine("you are on Junction #" + Jnumber);
+    Console.WriteLine("3 halls lie ahead...");
+    Console.WriteLine("only one will get you to class on time");
     Console.WriteLine("Which will you choose? (Type 1, 2, or 3)");
-    userRoom = Convert.ToInt32(Console.ReadLine());
+    userTurn = Convert.ToInt32(Console.ReadLine());
 
-    if (userRoom > 3) {
+    if (userTurn > 3) {
         ClearLine();
-        Console.WriteLine("that's not a door, try again");
+        Console.WriteLine("that's not a hall, try again");
         Thread.Sleep(2000);
         Console.Clear();
-        Entrypoint();
+    }
+   
+    WrongTurn = rnd.Next(1,3);
+    if (userTurn == WrongTurn) {
+        Console.WriteLine("You made a wrong turn and got a tardy :( ");
+       
     }
     else {
-        DoorChoice();
-    }
-}
-
-void DoorChoice() {
-    GhostRoom = rnd.Next(1,3);
-    if (userRoom == GhostRoom) {
-        Console.WriteLine("ghost");
-    }
-    else {
-        RoomNumber = RoomNumber +1;
-        Console.WriteLine("pass");
+        Jnumber = Jnumber +1;
+        Console.WriteLine("you made the correct turn!");
         Thread.Sleep(2000);
         Console.WriteLine("Let's keep going");
         Thread.Sleep(2000);
         Console.Clear();
-        Entrypoint();
+        
+    }
+
+    if (Jnumber > 10) {
+        continue;
     }
 }
 
-//Start function loop
-Entrypoint(); 
+if (Jnumber > 10) {
+    Console.WriteLine("You made it to class on time and got cookies! ");
+}
+else { 
+    Console.WriteLine("Let's try again tommorow");
+    Console.Write("Press <Enter> to exit... ");
+        while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+}
 
